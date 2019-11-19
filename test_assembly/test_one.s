8ec1ec5f1c244f66ad5ca74bcedfecc08425b56b
Codetector <codetector@codetector.cn>
2019-11-18 21:24:45 -0500

    binary

--- test_assembly/test_one.s
+++ test_assembly/test_one.s
@@ -1,8 +1,19 @@
         ORG     &H000       ; Lmao
-Start:  LOAD    B
+        JUMP Start
+        RETI
+        JUMP IRQ1
+        RETI
+        RETI
+Start:  SEI &HF
+        LOAD    B
         ADD     C
         STORE   A
+        OUT &H10
 Here:   JUMP    Here
+
+IRQ1:   LOADI &H69
+        OUT &H69
+        RETI
 
         ORG     &H010
 A:      DW      &H0000
